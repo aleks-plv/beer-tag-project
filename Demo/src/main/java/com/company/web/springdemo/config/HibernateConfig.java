19de54cc9a375c319e2a71a5b4b33c503dfa2927
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2024-01-12 13:28:28 +0200

    added HibernateConfig class

---
+++ Demo/src/main/java/com/company/web/springdemo/config/HibernateConfig.java
@@ -0,0 +1,61 @@
+package com.company.web.springdemo.config;
+
+import com.company.web.springdemo.repositories.StyleRepository;
+import com.company.web.springdemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class HibernateConfig {
+    private final String dbUrl, dbUsername, dbPassword;
+
+    @Autowired
+    public HibernateConfig(Environment env) {
+        this.dbUrl = env.getProperty("database.url");
+        this.dbUsername = env.getProperty("database.username");
+        this.dbPassword = env.getProperty("database.password");
+    }
+
+    @Bean(name="entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionfactory = new LocalSessionFactoryBean();
+        sessionfactory.setDataSource(dataSource());
+        //modelite, koito shte budat svurzani s tablicite
+        sessionfactory.setPackagesToScan("com.telerikacademy.web.springdemo.models");
+        //hibernate shte raboti na bazata na nastroikite v tozi metod
+        sessionfactory.setHibernateProperties(hibernateProperties());
+        return sessionfactory;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        //zadavame s kakuv driver da raboti
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        return dataSource;
+    }
+
+    private Properties hibernateProperties(){
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+        return hibernateProperties;
+
+    }
+
+
+}
